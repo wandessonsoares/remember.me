230fbf2dc94d243aaf5ab7a44d32f44d04a75003
wandessonsoares <wandessonsilvatsi@gmail.com>
2014-08-19 12:02:58 -0300

    Migrando para postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140819124402) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tarefas", force: true do |t|
     t.string   "titulo"
     t.string   "data"
